4f103c2e2a4970fe5650344b4e11618dc97ef033
tivadark <kamtivi@gmail.com>
2020-09-24 22:46:27 +0200

    Bool return value

--- Battle.h
+++ Battle.h
@@ -5,5 +5,5 @@
 class Battle {
 public:
 	static void StartFight(Warrior& warrior1, Warrior& warrior2);
-	static std::string GetAttackResult(Warrior& warriorA, Warrior& warriorB);
+	static bool GetAttackResult(Warrior& warriorA, Warrior& warriorB);
 };
