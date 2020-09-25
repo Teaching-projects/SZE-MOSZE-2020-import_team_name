31b3fbb180e607d41ce553524c3214e23fba9ead
tivadark <kamtivi@gmail.com>
2020-09-25 03:40:30 +0200

    Refactor class

--- Battle.cpp
+++ Battle.cpp
@@ -1,48 +1,45 @@
 #include "Battle.h"
+bool order = true;
 
-int orderDefault = true;
+void printingOut(Warrior& warrior1, Warrior& warrior2) {
+	std::cout << warrior1.getName() << +": HP: " << warrior1.getHealth() << +", DMG: " << warrior1.getDmg() << std::endl;
+	std::cout << warrior2.getName() << +": HP: " << warrior2.getHealth() << +", DMG: " << warrior2.getDmg() << std::endl;
+}
 
 void Battle::StartFight(Warrior& warrior1, Warrior& warrior2) {
+
+	Warrior* ptrA = &warrior1;
+	Warrior* ptrB = &warrior2;
+
 	while (true) {
-		if (Battle::GetAttackResult(warrior1, warrior2)) {
+		if (order) {
+			ptrA = &warrior1;
+			ptrB = &warrior2;
+			order = false;
+		}
+		else {
+			ptrA = &warrior2;
+			ptrB = &warrior1;
+			order = true;
+		}
+
+		if (Battle::GetAttackResult(*ptrA, *ptrB)) {
 			break; //kill the entire loop and end the program
 		}
-		if (Battle::GetAttackResult(warrior2, warrior1)) { //warrior1 is gonna get the chance to attack warrior2
-			break;
-		}
+		printingOut(warrior1, warrior2);
+		std::cout << (*ptrA).getName() << " -> " << (*ptrB).getName() << std::endl;
 	}
 }
 
 bool Battle::GetAttackResult(Warrior& warriorA, Warrior& warriorB) {
-	if (orderDefault) {
-		std::cout << warriorA.getName() << +": HP: " << warriorA.getHealth() << +", DMG: " << warriorA.getDmg() << std::endl;
-		std::cout << warriorB.getName() << +": HP: " << warriorB.getHealth() << +", DMG: " << warriorB.getDmg() << std::endl;
-		orderDefault = false;
-	}
-	else {
-		std::cout << warriorB.getName() << +": HP: " << warriorB.getHealth() << +", DMG: " << warriorB.getDmg() << std::endl;
-		std::cout << warriorA.getName() << +": HP: " << warriorA.getHealth() << +", DMG: " << warriorA.getDmg() << std::endl;
-		orderDefault = true;
-	}
-	std::cout << warriorA.getName() << " -> " << warriorB.getName() << std::endl;
-
 	int warriorAAttkAmt = warriorA.Attack();
 	int damage2WarriorB = warriorAAttkAmt;
 	damage2WarriorB = (damage2WarriorB <= 0) ? 0 : damage2WarriorB;
 
 	warriorB.setHealth(((warriorB.getHealth() - damage2WarriorB) <= 0) ? 0 : (warriorB.getHealth() - damage2WarriorB));
 
-
-
 	if (warriorB.getHealth() <= 0) { //if any of the Warrior dies then print out at the end...
-		if (orderDefault) {
-			std::cout << warriorB.getName() << +": HP: " << warriorB.getHealth() << +", DMG: " << warriorB.getDmg() << std::endl;
-			std::cout << warriorA.getName() << +": HP: " << warriorA.getHealth() << +", DMG: " << warriorA.getDmg() << std::endl;
-		}
-		else {
-			std::cout << warriorA.getName() << +": HP: " << warriorA.getHealth() << +", DMG: " << warriorA.getDmg() << std::endl;
-			std::cout << warriorB.getName() << +": HP: " << warriorB.getHealth() << +", DMG: " << warriorB.getDmg() << std::endl;
-		}
+		printingOut(warriorA, warriorB);
 		std::cout << warriorB.getName() << " died. " << warriorA.getName() << " wins." << std::endl;
 		return true;
 	}
